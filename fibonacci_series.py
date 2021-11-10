cc41231c240f962ea9160a80687de3cb9f0d1c4d
JoyPatel24 <72862865+JoyPatel24@users.noreply.github.com>
2021-11-10 16:31:06 +0530

    Assignment 1(A)

---
+++ fibonacci_series.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''
+Write a Python program to get the Fibonacci series between 0 to 50
+
+
+
+Note : The Fibonacci Sequence is the series of numbers :
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+
+Every next number is found by adding up the two numbers before it.
+
+Expected Output : 1 1 2 3 5 8 13 21 34
+'''
+
+a,b=0,1
+while b<50:
+    print(b)
+    a,b=b,a+b
+
+
+# In[ ]:
+
+
+
+
