bb531916ff8a289cdce43de3963c7b848b0364c9
JoyPatel24 <72862865+JoyPatel24@users.noreply.github.com>
2021-11-10 16:43:33 +0530

    Assignment 1(B)

---
+++ Mirror images.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+'''
+Write a Python program that accepts a word from the user and reverse it.
+
+
+
+Sample Test Case
+
+
+
+Input : Edyoda
+
+output: adoydE
+'''
+name = 'Edyoda'
+ 
+for char in range(len(name) - 1, -1, -1):
+  print(name[char], end="")
+
+
+
+# In[ ]:
+
+
+
+
