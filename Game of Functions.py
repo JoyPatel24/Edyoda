fbf1d7d55fb4f45399de96f6944358908d633d49
JoyPatel24 <72862865+JoyPatel24@users.noreply.github.com>
2021-11-21 18:12:40 +0530

    Assignment 3(A)

---
+++ Game of Functions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+'''
+Write a Python function to sum all the numbers in a list.
+
+
+
+Sample List : (8, 2, 3, 0, 7)
+
+Expected Output : 20
+'''
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+
+# In[ ]:
+
+
+
+
